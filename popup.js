b08147414381a2c72c098af624bc03f6548b6994
William Seymour <w.r.seymour@warwick.ac.uk>
2016-12-01 15:46:29 +0000

    updated to work with recent API changes

--- popup.js
+++ popup.js
@@ -1,29 +1,35 @@
 function getCurrentTabUrl(callback) {
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-  chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0];
-    var url = tab.url;
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-    callback(url);
-  });
-}
+	var queryInfo = {
+		active: true,
+		currentWindow: true
+	};
+	chrome.tabs.query(queryInfo, function(tabs) {
+		var tab = tabs[0];
+		var url = tab.url;
+		console.assert(typeof url == 'string', 'tab.url should be a string');
+		callback(url);
+	});
+};
 
-function setSent() {
-  document.getElementById('status').innerHTML = 'Done!';
+function setText(text) {
+  document.getElementById('status').textContent = text;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    var postData = 'url=' + url;
-    var request = new XMLHttpRequest();
-
-    request.onload = function () {}
-    request.open('POST', 'http://music.lan:8080/url', true);
-    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-    request.send(postData);
-    setSent();
-  });
+	getCurrentTabUrl(function(url) {
+		var request = new XMLHttpRequest();
+    	var postData = 'url=' + url;
+    	request.open('POST', 'http://music.lan/api/url', true);
+    	request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+		request.onload = function() {
+			if (request.status == 200) {
+				setText("Done!");	
+			} else if (request.status == 503) {
+				setText("Server offline!");
+			} else {
+				setText("Bad request!");
+			}
+		}
+    	request.send(postData);
+	})
 });
